819e1fd3c55abc523784ce06a61cafed15322c0d
rafajlpz <frilo_94@hotmail.com>
2022-02-26 22:02:38 +0100

    Destructurando objetos

---
+++ JsUdemy/objetos.js
@@ -0,0 +1,19 @@
+
+const web =  {
+    nombre: 'sdfasfds',
+    links: {
+        enlace: 'www.bluuweb.cl'
+    },
+    redesSociales:{
+        youtube: {
+            enlace: 'youtube.com/bluuweb',
+            nombre: 'bluuweb yt'
+        }
+    }
+}
+
+
+
+const {nombre, enlace} = web.redesSociales.youtube
+
+console.log(nombre)
