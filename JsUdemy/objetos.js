923540ae0fb114acd64c7ff6b6d2cf9e3a30579d
rafajlpz <frilo_94@hotmail.com>
2022-02-26 22:59:34 +0100

    Uso de fetch() y transformacion a json

--- JsUdemy/objetos.js
+++ JsUdemy/objetos.js
@@ -1,19 +1,12 @@
 
-const web =  {
-    nombre: 'sdfasfds',
-    links: {
-        enlace: 'www.bluuweb.cl'
-    },
-    redesSociales:{
-        youtube: {
-            enlace: 'youtube.com/bluuweb',
-            nombre: 'bluuweb yt'
-        }
-    }
-}
+fetch ('https://pokeapi.co/api/v2/pokemon') // CONSUME UNA API, UNA URL EN ESTE CASO
+    .then ( res => res.json())  // LA TRANSFORMAMOS EN JSON
 
+    .then(data => {
+        
+        data.results.forEach(element => {
+            console.log(element.name)
+        });
+    })
 
-
-const {nombre, enlace} = web.redesSociales.youtube
-
-console.log(nombre)
+    .catch(error => console.log(error))
