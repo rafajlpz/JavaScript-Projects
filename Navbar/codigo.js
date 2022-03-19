12240251a84b0eb79bb81b9424e37ae1d430f40b
rafajlpz <frilo_94@hotmail.com>
2022-03-19 21:21:08 +0100

    Navbar
    -Creacion de un menu navegable con fontawesome y js
    -Copiado codigo css@

---
+++ Navbar/codigo.js
@@ -0,0 +1,14 @@
+const navToogle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+const clickToogle = (navToogle.onclick = () => {
+  // console.log(links.classList);
+  // console.log(links.classList.contains('random'));
+  // console.log(links.classList.contains('links'));
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //       links.classList.add('show-links')
+  //   }
+  links.classList.toggle("show-links");
+});
