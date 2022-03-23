1babb3a00a69a53742c6f72891b814654a727a8f
rafajlpz <frilo_94@hotmail.com>
2022-03-23 18:40:10 +0100

    Creacion Sidebar
    Codigo limpio js

---
+++ Sidebar/codigo.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+const MonstrarSidebar = () => {
+  toggleBtn.onclick = () => {
+    console.log(sidebar.classList);
+    // if (sidebar.classList.contains("show-sidebar")) {
+    //   sidebar.classList.remove("show-sidebar");
+    // } else {
+    //   sidebar.classList.add("show-sidebar");
+    // }
+    sidebar.classList.toggle("show-sidebar");
+  };
+};
+
+const cerrarBoton = () => {
+  closeBtn.onclick = () => {
+    sidebar.classList.remove("show-sidebar");
+  };
+};
+
+cerrarBoton();
+MonstrarSidebar();
