f586983f3d5f60963bb5c7a8795dbbc1a0ed0511
rafajlpz <frilo_94@hotmail.com>
2022-03-24 14:17:30 +0100

    Codigo js
    Cogido los botones abrir y cerrar

--- Modal/codigo.js
+++ Modal/codigo.js
@@ -1,2 +1,13 @@
 "use strict";
 
+const btnAbrir = document.querySelector(".boton-abrir-modal");
+const btnCerrar = document.querySelector(".cerrar-modal");
+// document.querySelector(".abrir-modal")
+
+const abrirFicha = btnAbrir.onclick = () => {
+    alert()
+}
+
+const cerrarFicha = btnCerrar.onclick = () =>{
+    alert()
+}
